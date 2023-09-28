a6dc5d9d8143e5b13524d8dae0421f965404f7c1
sepxx <mihartthx@gmail.com>
2023-09-28 12:03:21 +0300

    init commit

---
+++ currencyRateClient/src/test/java/com/bnk/currencyrateclient/CurrencyRateClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnk.currencyrateclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
