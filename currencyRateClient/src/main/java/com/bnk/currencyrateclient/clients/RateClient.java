34e8996b27d46b3b9ad8f01788f528181257ac70
sepxx <mihartthx@gmail.com>
2023-09-28 14:03:37 +0300

    currecnyRateClient: EndpointService

--- currencyRateClient/src/main/java/com/bnk/currencyrateclient/clients/RateClient.java
+++ currencyRateClient/src/main/java/com/bnk/currencyrateclient/clients/RateClient.java
@@ -2,6 +2,8 @@ package com.bnk.currencyrateclient.clients;
 
 import com.bnk.currencyrateclient.model.CurrencyRate;
 
+import java.time.LocalDate;
+
 public interface RateClient {
-    public CurrencyRate getCurrencyRate();
+    public CurrencyRate getCurrencyRate(String currency, LocalDate date);
 }
---
+++ currencyRateClient/src/main/java/com/bnk/currencyrateclient/services/CurrencyRateEndpointService.java
@@ -0,0 +1,27 @@
+package com.bnk.currencyrateclient.services;
+
+import com.bnk.currencyrateclient.clients.RateClient;
+import com.bnk.currencyrateclient.model.CurrencyRate;
+import com.bnk.currencyrateclient.model.RateType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class CurrencyRateEndpointService {
+
+    private final Map<String, RateClient> clients;
+
+    public CurrencyRateEndpointService(Map<String, RateClient> clients) {
+        this.clients = clients;
+    }
+
+    public CurrencyRate getCurrencyRate(RateType rateType, String currency, LocalDate date) {
+        log.info("getCurrencyRate. rateType:{}, currency:{}, date:{}", rateType, currency, date);
+        RateClient rateClient = clients.get(rateType.getServiceName());
+        return rateClient.getCurrencyRate(currency, date);
+    }
+}
